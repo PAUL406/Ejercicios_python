6da07d274b87a60e8ac3fbe0ce173f5da2f60a84
Paul <35155282+PAUL406@users.noreply.github.com>
2021-07-24 16:15:16 -0400

    Add files via upload

---
+++ ejercicio_python/code.py
@@ -0,0 +1,31 @@
+# Primero que todo buenos días/tarde/noche cuando estés viendo esto :D 
+from io import open
+import os,fnmatch,shutil,errno
+extensiones = ['*.jpg', '*.png'] 
+directorios = ['/home/paul/Escritorio/ejercicio_python/pseudo_tarea/A','/home/paul/Escritorio/ejercicio_python/pseudo_tarea/B'] 
+
+lista2=[]
+for i in directorios: 
+
+    lista = [os.path.join(raiz, archivos)
+        for raiz, _, archivos in os.walk(i)
+            for ext in extensiones
+                for archivos in fnmatch.filter(archivos, ext)]
+    lista2.extend(lista)
+
+newDir='lo_que_copio'
+try:
+    os.mkdir(newDir)
+except OSError as e:
+    if e.errno != errno.EEXIST:
+        raise
+
+
+archivo_externo=open('externo.txt','w') 
+for i in lista2:
+    shutil.copy(i,newDir)
+    archivo_externo.write(i)
+    archivo_externo.write('\n')
+
+archivo_externo.close()
+
