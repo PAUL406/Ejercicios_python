9a1da6954ea91ea3c1d51f4ef90fb706b7fecbdd
Paul <35155282+PAUL406@users.noreply.github.com>
2021-07-24 16:32:22 -0400

    Update code.py

--- ejercicio_python/code.py
+++ ejercicio_python/code.py
@@ -1,7 +1,7 @@
 # Primero que todo buenos días/tarde/noche cuando estés viendo esto :D 
 from io import open
 import os,fnmatch,shutil,errno
-extensiones = ['*.jpg', '*.png'] #Extensiones que quiero que encientre 
+extensiones = ['*.jpg', '*.png'] #Extensiones que quiero que encuentre 
 directorios = ['/home/paul/Escritorio/ejercicio_python/pseudo_tarea/A','/home/paul/Escritorio/ejercicio_python/pseudo_tarea/B'] #directorios en los que buscara
 
 lista2=[]
